0aa0d9759e3dacbf9af794644fc6e2f1edc736bd
billavramidis <bavramidis04@gmail.com>
2024-07-10 12:14:10 +0300

    Minor fixes

--- src/HelpScreen.java
+++ src/HelpScreen.java
@@ -33,7 +33,7 @@ public class HelpScreen extends JFrame {
         title.setForeground(Color.WHITE);
     
         //Adds the info.
-        creditsTextArea = new JTextArea(languagePack.credits);
+        creditsTextArea = new JTextArea(languagePack.help);
         creditsTextArea.setFont(new Font("Arial", Font.PLAIN, 20));
         creditsTextArea.setForeground(Color.WHITE);
         creditsTextArea.setBackground(new Color(0, 0, 0, 0));
@@ -126,7 +126,7 @@ public class HelpScreen extends JFrame {
 
     //This class contains all the text of the screen in both languages and makes the switch between the two languages.
     public class LanguagePack {
-        private String credits;
+        private String help;
         private String windowTitle;
         private String title;
         private String returnButtonText;
@@ -134,14 +134,14 @@ public class HelpScreen extends JFrame {
         //This method takes a String as its attribute and if it is "en" it chooses the English text, otherwise it chooses the Greek text.
         public void loadLanguage(String language) {
             if (language.equals("en")) {
-                credits = "ARMADA PATROLS: THE GAME\n\n" +
+                help = "ARMADA PATROLS: THE GAME\n\n" +
                         "Login/Register\n" +
-                        "When entering the game, the user can either log into an existing account or create a new one by pressing LOGIN/REGISTER. " +
-                        "This leads to the Welcome Screen, where the user can choose either Login or Register. (The benefit of logging in is access to Statistics.)\n\n" +
+                        "When entering the game, the user is greeted by the Welcome Screen where they can either log into an existing account or create a new one by pressing I'M A COMMANDER/CREATE COMMANDER. " +
+                        "This leads to the Login or the Register Screen respectively, where they can enter their credentials to continue. Αlternatively, they can press CONTINUE AS GUEST to start playing without logging on. (The benefit of logging in is access to Statistics.)\n\n" +
                         "Game Setup\n" +
-                        "The user can press Start Game and select the game difficulty (Easy – Medium – Hard), to proceed to the Pre-Game Screen. " +
+                        "The user can press Start Game and select the game difficulty (EASY – MEDIUM – HARD), to proceed to the Pre-Game Screen. " +
                         "There, the user must place their ships by dragging and dropping them into the grid. " +
-                        "The ships are initially horizontal in the left column but can be rotated 90 degrees by pressing the ROTATE Button to place them vertically. " +
+                        "The ships are initially horizontal in the left column but can be rotated 90 degrees by pressing the ROTATE Button before they procceed with the placement to place them vertically." +
                         "Once the user has placed all 5 ships, they can press START THE GAME to begin their Patrol!\n\n" +
                         "Game Start\n" +
                         "On the game screen, the user and the computer take turns. " +
@@ -151,45 +151,41 @@ public class HelpScreen extends JFrame {
                         "Next, the computer fires at the user's grid. If it hits a ship, the corresponding part of the ship appears burned; otherwise, a splash appears. " +
                         "The winner is the one who sinks all the opponent's ships first. A win/loss message appears to the user. " +
                         "At any time during the game, the user can press RETURN to go back to the main screen.\n\n" +
-                        "Game Continuation\n" +
-                        "If the game is interrupted, the user can press CONTINUE GAME from the main screen to resume from where they left off.\n\n" +
-                        "Powerups\n" +
-                        "During a game, the user can choose to use a powerup to fire with it.\n\n" +
+                        "Powerup\n" +
+                        "During a game, the user can choose a 2x2 bomb powerup to fire with it.\n\n" +
                         "Settings\n" +
                         "From the main screen, the user can press OPTIONS to go to the settings screen, where they can change the language from English to Greek and vice versa. " +
                         "They can also view the credits.\n\n" +
                         "Statistics\n" +
-                        "From the main screen, if logged in, the user can press STATISTICS to view their stats, such as wins/losses, times each Powerup was used, total shots against the opponent, etc.\n";
+                        "From the main screen, if logged in, the user can press STATISTICS to view their stats, such as wins/losses and successful shots percentage.\n";
                 windowTitle = "Help";
                 title = "HELP";
                 returnButtonText = "RETURN";
             } else {
-                credits = "ARMADA PATROLS: THE GAME\n\n" +
-                        "Login/Register\n" +
-                        "Ο χρήστης όταν μπαίνει στο παιχνίδι μπορεί να συνδεθεί σε υπάρχοντα λογαριασμό του ή να δημιουργήσει καινούργιο λογαριασμό πατώντας το LOGIN/REGISTER. " +
-                        "Έτσι μεταβαίνει στην οθόνη Welcome Screen, και έχει την επιλογή είτε για Login, είτε για Register. (Το προνόμιο της σύνδεσης είναι η πρόσβαση σε Statistics.)\n\n" +
-                        "Προετοιμασία παρτίδας\n" +
-                        "Ο χρήστης μπορεί να πατήσει Start Game και να διαλέξει δυσκολία παρτίδας (Εύκολο – Μεσαίο – Δύσκολο), προκειμένου να μεταβεί στο Pre-Game Screen. " +
-                        "Εκεί, πρέπει να τοποθετήσει τα πλοία του κάνοντάς τα drag-and-drop στο πλαίσιο. " +
-                        "Τα πλοία βρίσκονται αρχικά οριζόντια στην αριστερή στήλη, αλλά μπορούν να περιστραφούν 90ο πατώντας το ROTATE Button προκειμένου να τοποθετηθούν κάθετα. " +
-                        "Όταν ο χρήστης τοποθετήσει και τα 5 πλοία, μπορεί να πατήσει START THE GAME για να ξεκινήσει το Patrol του!\n\n" +
-                        "Έναρξη παρτίδας\n" +
-                        "Στην οθόνη της παρτίδας, ο χρήστης και ο υπολογιστής παίζουν εναλλάξ. " +
-                        "Το grid του χρήστη βρίσκεται στα αριστερά και εκεί φαίνονται τα πλοία του. Το grid του υπολογιστή είναι στα δεξιά και εκεί θα φαίνονται όσα πλοία αποκαλύψει ο χρήστης. " +
-                        "Πρώτος ξεκινά ο χρήστης, πατώντας σε οποιοδήποτε από τα κελιά του grid του αντίπαλου. " +
-                        "Αν στο κελί αυτό υπάρχει πλοίο, εμφανίζεται έκρηξη στον χρήστη, ενώ αν δεν υπάρχει εμφανίζεται απλά πιτσιλιά ώστε να γνωρίζει ότι έχει πυροβολήσει εκεί. " +
-                        "Σειρά έχει ο υπολογιστής, ο οποίος πυροβολεί στο grid του χρήστη. Αν βρει στόχο εμφανίζεται το αντίστοιχο κομμάτι του πλοίου καμένο, ενώ αλλιώς εμφανίζεται πιτσιλιά. " +
-                        "Ο νικητής της παρτίδας είναι αυτός που βυθίζει πρώτος όλα τα αντίπαλα πλοία. Εμφανίζεται αντίστοιχο μήνυμα νίκης/ήττας στον χρήστη. " +
-                        "Ανά πάσα στιγμή της παρτίδας, ο χρήστης μπορεί να πατήσει RETURN για να γυρίσει στην αρχική οθόνη.\n\n" +
-                        "Συνέχιση παρτίδας\n" +
-                        "Σε περίπτωση λοιπόν όπου διακοπεί πρόωρα το παιχνίδι, ο χρήστης μπορεί να πατήσει CONTINUE GAME από την αρχική οθόνη και να συνεχίσει το παιχνίδι από εκεί που το άφησε.\n\n" +
-                        "Powerups\n" +
-                        "Κατά τη διάρκεια μιας παρτίδας, ο χρήστης μπορεί να επιλέξει τη χρήση ενός powerup και να πυροβολήσει με αυτό.\n\n" +
+                help = "ΠΕΡΙΠΟΛΙΕΣ ΑΡΜΑΔΑΣ: ΤΟ ΠΑΙΧΝΙΔΙ\n\n" +
+                        "Σύνδεση/Εγγραφή\n" +
+                        "Όταν εισέρχεται στο παιχνίδι, ο χρήστης χαιρετίζεται από την Οθόνη Υποδοχής όπου μπορεί είτε να συνδεθεί σε υπάρχοντα λογαριασμό είτε να δημιουργήσει νέο πατώντας ΕΙΜΑΙ ΔΙΟΙΚΗΤΗΣ/ΔΗΜΙΟΥΡΓΙΑ ΔΙΟΙΚΗΤΗ. " +
+                        "Αυτό οδηγεί στην Οθόνη Σύνδεσης ή στην Οθόνη Εγγραφής αντίστοιχα, όπου μπορούν να εισάγουν τα στοιχεία τους για να συνεχίσουν. Εναλλακτικά, μπορούν να πατήσουν ΣΥΝΕΧΕΙΑ ΩΣ ΕΠΙΣΚΕΠΤΗΣ για να αρχίσουν να παίζουν χωρίς να συνδεθούν. (Το πλεονέκτημα της σύνδεσης είναι η πρόσβαση στα Στατιστικά.)\n\n" +
+                        "Ρύθμιση Παιχνιδιού\n" +
+                        "Ο χρήστης μπορεί να πατήσει Έναρξη Παιχνιδιού και να επιλέξει τη δυσκολία του παιχνιδιού (ΕΥΚΟΛΟ – ΜΕΣΑΙΟ – ΔΥΣΚΟΛΟ), για να προχωρήσει στην Προ-Παιχνιδιού Οθόνη. " +
+                        "Εκεί, ο χρήστης πρέπει να τοποθετήσει τα πλοία του σύροντας και αφήνοντάς τα στο πλέγμα. " +
+                        "Τα πλοία είναι αρχικά οριζόντια στη αριστερή στήλη αλλά μπορούν να περιστραφούν κατά 90 μοίρες πατώντας το κουμπί ΠΕΡΙΣΤΡΟΦΗ πριν συνεχίσουν με την τοποθέτηση για να τα τοποθετήσουν κάθετα." +
+                        "Μόλις ο χρήστης τοποθετήσει και τα 5 πλοία, μπορεί να πατήσει ΕΝΑΡΞΗ ΤΟΥ ΠΑΙΧΝΙΔΙΟΥ για να ξεκινήσει την Περιπολία του!\n\n" +
+                        "Έναρξη Παιχνιδιού\n" +
+                        "Στην οθόνη του παιχνιδιού, ο χρήστης και ο υπολογιστής παίζουν εναλλάξ. " +
+                        "Το πλέγμα του χρήστη είναι στα αριστερά, δείχνοντας τα πλοία του. Το πλέγμα του υπολογιστή είναι στα δεξιά, δείχνοντας όποια πλοία αποκαλύπτει ο χρήστης. " +
+                        "Ο χρήστης ξεκινά πρώτος πατώντας σε οποιοδήποτε κελί στο πλέγμα του αντιπάλου. " +
+                        "Αν υπάρχει πλοίο σε αυτό το κελί, εμφανίζεται έκρηξη· αλλιώς, εμφανίζεται ένα κύμα για να υποδηλώσει ότι πυροβολήθηκε εκεί. " +
+                        "Στη συνέχεια, ο υπολογιστής πυροβολεί στο πλέγμα του χρήστη. Αν χτυπήσει πλοίο, το αντίστοιχο μέρος του πλοίου εμφανίζεται καμένο· αλλιώς, εμφανίζεται ένα κύμα. " +
+                        "Ο νικητής είναι αυτός που θα βυθίσει πρώτος όλα τα πλοία του αντιπάλου. Εμφανίζεται ένα μήνυμα νίκης/ήττας στον χρήστη. " +
+                        "Σε οποιαδήποτε στιγμή κατά τη διάρκεια του παιχνιδιού, ο χρήστης μπορεί να πατήσει ΕΠΙΣΤΡΟΦΗ για να επιστρέψει στην κύρια οθόνη.\n\n" +
+                        "Ενίσχυση\n" +
+                        "Κατά τη διάρκεια του παιχνιδιού, ο χρήστης μπορεί να επιλέξει μια ενίσχυση βόμβας 2x2 για να πυροβολήσει με αυτήν.\n\n" +
                         "Ρυθμίσεις\n" +
-                        "Από την αρχική οθόνη ο χρήστης μπορεί να πατήσει OPTIONS προκειμένου να μεταβεί στην οθόνη των ρυθμίσεων, απ’ όπου μπορεί να αλλάξει γλώσσα από αγγλικά σε ελληνικά και αντιστρόφως. " +
-                        "Επίσης μπορεί να να δει τα credits.\n\n" +
+                        "Από την κύρια οθόνη, ο χρήστης μπορεί να πατήσει ΕΠΙΛΟΓΕΣ για να πάει στην οθόνη ρυθμίσεων, όπου μπορεί να αλλάξει τη γλώσσα από Αγγλικά σε Ελληνικά και αντίστροφα. " +
+                        "Μπορεί επίσης να δει τα credits.\n\n" +
                         "Στατιστικά\n" +
-                        "Από την αρχική οθόνη ο χρήστης, αν είναι συνδεδεμένος, μπορεί να πατήσει STATISTICS και να δει τα στατιστικά του στοιχεία, π.χ. νίκες/ήττες, φορές χρήσης του κάθε Powerup, συνολικές βολές ενάντια στον αντίπαλο, κλπ.\n";
+                        "Από την κύρια οθόνη, αν είναι συνδεδεμένος, ο χρήστης μπορεί να πατήσει ΣΤΑΤΙΣΤΙΚΑ για να δει τα στατιστικά του, όπως νίκες/ήττες και ποσοστό επιτυχών βολών.\n";
                 windowTitle = "Βοήθεια";
                 title = "ΒΟΗΘΕΙΑ";
                 returnButtonText = "ΕΠΙΣΤΡΟΦΗ";
